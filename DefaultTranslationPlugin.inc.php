e66831d9c29282f9390be29c939a90b7bd3ad983
Jonas Raoni Soares da Silva <github@raoni.org>
2022-03-09 00:36:37 +0300

    #9 Formatting

--- DefaultTranslationPlugin.inc.php
+++ DefaultTranslationPlugin.inc.php
@@ -3,14 +3,14 @@
 /**
  * @file DefaultTranslationPlugin.inc.php
  *
- * Copyright (c) 2013-2020 Simon Fraser University
- * Copyright (c) 2003-2020 John Willinsky
+ * Copyright (c) 2013-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @package plugins.generic.defaultTranslation
  * @class DefaultTranslationPlugin
  *
- * Display English translation if the current UI language translation doesn't exist
+ * Fallbacks to an English translation if the requested locale key isn't translated for the current locale
  */
 
 use PKP\facades\Locale;
@@ -18,55 +18,60 @@ use PKP\i18n\interfaces\LocaleInterface;
 use PKP\plugins\GenericPlugin;
 use PKP\plugins\HookRegistry;
 
-class DefaultTranslationPlugin extends GenericPlugin {
-	/**
-	 * @copydoc PKPPlugin::getDisplayName()
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.defaultTranslation.displayName');
-	}
+class DefaultTranslationPlugin extends GenericPlugin
+{
+    /**
+     * @copydoc PKPPlugin::getDisplayName()
+     */
+    public function getDisplayName(): string
+    {
+        return __('plugins.generic.defaultTranslation.displayName');
+    }
 
-	/**
-	 * @copydoc PKPPlugin::getDescription()
-	 */
-	function getDescription() {
-		return __('plugins.generic.defaultTranslation.description');
-	}
+    /**
+     * @copydoc PKPPlugin::getDescription()
+     */
+    public function getDescription(): string
+    {
+        return __('plugins.generic.defaultTranslation.description');
+    }
 
-	/**
-	 * @copydoc LazyLoadPlugin::register()
-	 */
-	function register($category, $path, $mainContextId = null) {
-		$success = parent::register($category, $path, $mainContextId);
-		if ($success && $this->getEnabled()) {
-			HookRegistry::register('Locale::translate', fn () => $this->translate(...func_get_args()));
-		}
-		return $success;
-	}
+    /**
+     * @copydoc LazyLoadPlugin::register()
+     *
+     * @param null|mixed $mainContextId
+     */
+    public function register($category, $path, $mainContextId = null): bool
+    {
+        $success = parent::register($category, $path, $mainContextId);
+        if ($success && $this->getEnabled()) {
+            HookRegistry::register('Locale::translate', fn () => $this->translate(...func_get_args()));
+        }
+        return $success;
+    }
 
-	/**
-	 * @copydoc PKPPlugin::getSeq()
-	 */
-	function getSeq() {
-		return -1;
-	}
+    /**
+     * @copydoc PKPPlugin::getSeq()
+     */
+    public function getSeq(): int
+    {
+        return -1;
+    }
 
-	/**
-	 * Hook callback: Handle requests.
-	 * Show English translation if the current UI language translation doesn't exist.
-	 * @param $hookName string The name of the hook being invoked
-	 * @param $args array The parameters to the invoked hook
-	 */
-	function translate($hookName, $args) {
-		[&$value, $key, $params, $number, $locale, $localeBundle] = $args;
+    /**
+     * Attempts to find a translation for the requested locale key using the default locale (en_US)
+     */
+    public function translate(string $hookName, array $args): bool
+    {
+        [&$value, $key, $params, $number, $locale, $localeBundle] = $args;
 
-		if ($locale === LocaleInterface::DEFAULT_LOCALE) {
-			return false;
-		}
+        if ($locale === LocaleInterface::DEFAULT_LOCALE) {
+            return false;
+        }
 
-		$value = $number === null
-			? Locale::get($key, $params, LocaleInterface::DEFAULT_LOCALE)
-			: Locale::choice($key, $number, $params, LocaleInterface::DEFAULT_LOCALE);
-		return true;
-	}
+        $value = $number === null
+            ? Locale::get($key, $params, LocaleInterface::DEFAULT_LOCALE)
+            : Locale::choice($key, $number, $params, LocaleInterface::DEFAULT_LOCALE);
+        return true;
+    }
 }
